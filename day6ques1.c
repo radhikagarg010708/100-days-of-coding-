08e029a9938027bac6807319aab15d0c4a1b865d
radhikagarg010708 <radhikagarg010708@gmail.com>
2026-09-04 22:36:15 +0530

    DAY 6

---
+++ day6ques1.c
@@ -0,0 +1,22 @@
+//Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    if (n % 2 == 0)
+    {
+        printf("The number is even.");
+    }
+    else
+    {
+        printf("The number is odd.");
+    }
+
+    return 0;
+}
---
+++ day6ques2.c
@@ -0,0 +1,29 @@
+//Q12: Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    if (n >= 0)
+    {
+        if (n == 0)
+        {
+            printf("The number is zero.");
+        }
+        else
+        {
+            printf("The number is positive.");
+        }
+    }
+    else
+    {
+        printf("The number is negative.");
+    }
+
+    return 0;
+}
